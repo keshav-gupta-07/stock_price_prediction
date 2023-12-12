da9d13c9ceece38466c69246bcd7455c76cb6b1b
Keshav Gupta <guptakeshav0710@gmail.com>
2023-12-13 01:25:39 +0530

    First commit

---
+++ file.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize']=20,10
+
+from keras.models import Sequential
+from keras.layers import LSTM,Dropout,Dense
+from sklearn.preprocessing import MinMaxScaler
+
+df=pd.read_csv("NSE-TATA.csv")
+df.head()
+
+df["Date"]=pd.to_datetime(df.Date,format="%Y-%m-%d")
+df.index=df['Date']
+
+plt.figure(figsize=(16,8))
+plt.plot(df["Close"],label='Close Price history')
+
